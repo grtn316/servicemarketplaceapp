b68b0f4a2fd8d48d49fb9bfbaf3c886a2399e37c
David Baumgarten <grtn316@hotmail.com>
2024-06-11 18:47:30 -0500

    Initial Commit

---
+++ ServiceMarketplace/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceMarketplace.Data;
+using ServiceMarketplace.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceMarketplace.Repository
+{
+    public interface IServiceMarketplaceRepository
+    {
+        Task<IEnumerable<WeatherForecast>> GetAllAsync();
+        Task<WeatherForecast> GetByIdAsync(int id);
+        Task AddAsync(WeatherForecast entity);
+        Task UpdateAsync(WeatherForecast entity);
+        Task DeleteAsync(int id);
+    }
+
+    public class ServiceMarketplaceRepository : IServiceMarketplaceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiceMarketplaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetAllAsync()
+        {
+            return await _context.WeatherForecasts.ToListAsync();
+        }
+
+        public async Task<WeatherForecast> GetByIdAsync(int id)
+        {
+            return await _context.WeatherForecasts.FindAsync(id);
+        }
+
+        public async Task AddAsync(WeatherForecast entity)
+        {
+            await _context.WeatherForecasts.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(WeatherForecast entity)
+        {
+            _context.WeatherForecasts.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.WeatherForecasts.FindAsync(id);
+            if (entity != null)
+            {
+                _context.WeatherForecasts.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ServiceMarketplace/database/DBContext.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceMarketplace.Entities;
+
+namespace ServiceMarketplace.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<WeatherForecast>().HasData(
+                new WeatherForecast
+                {
+                    Id = 1,
+                    Date = new DateOnly(2023, 6, 1),
+                    TemperatureC = -5,
+                    Summary = "Freezing"
+                },
+                new WeatherForecast
+                {
+                    Id = 2,
+                    Date = new DateOnly(2023, 6, 2),
+                    TemperatureC = 0,
+                    Summary = "Bracing"
+                },
+                new WeatherForecast
+                {
+                    Id = 3,
+                    Date = new DateOnly(2023, 6, 3),
+                    TemperatureC = 5,
+                    Summary = "Chilly"
+                },
+                new WeatherForecast
+                {
+                    Id = 4,
+                    Date = new DateOnly(2023, 6, 4),
+                    TemperatureC = 10,
+                    Summary = "Cool"
+                },
+                new WeatherForecast
+                {
+                    Id = 5,
+                    Date = new DateOnly(2023, 6, 5),
+                    TemperatureC = 15,
+                    Summary = "Mild"
+                },
+                new WeatherForecast
+                {
+                    Id = 6,
+                    Date = new DateOnly(2023, 6, 6),
+                    TemperatureC = 20,
+                    Summary = "Warm"
+                },
+                new WeatherForecast
+                {
+                    Id = 7,
+                    Date = new DateOnly(2023, 6, 7),
+                    TemperatureC = 25,
+                    Summary = "Balmy"
+                },
+                new WeatherForecast
+                {
+                    Id = 8,
+                    Date = new DateOnly(2023, 6, 8),
+                    TemperatureC = 30,
+                    Summary = "Hot"
+                },
+                new WeatherForecast
+                {
+                    Id = 9,
+                    Date = new DateOnly(2023, 6, 9),
+                    TemperatureC = 35,
+                    Summary = "Sweltering"
+                },
+                new WeatherForecast
+                {
+                    Id = 10,
+                    Date = new DateOnly(2023, 6, 10),
+                    TemperatureC = 40,
+                    Summary = "Scorching"
+                }
+            );
+        }
+    }
+}
