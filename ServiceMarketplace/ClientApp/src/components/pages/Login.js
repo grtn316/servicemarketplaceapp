b8cc398fe256debfa83e856e1241fee23d236e1c
Shawn <thatbastardshawn@gmail.com>
2024-07-09 21:46:39 -0400

    Added initial login HTML
    Username label, username input element.
    Password label, password input element.
    Submit button.
    Currently nonfunctioning. No styles added.

--- ServiceMarketplace/ClientApp/src/components/pages/Login.js
+++ ServiceMarketplace/ClientApp/src/components/pages/Login.js
@@ -6,7 +6,17 @@ export class Login extends Component {
     render() {
         return (
             <>
-                
+                <div className="loginContainer">
+                    <label for="username">Username</label>
+                    <input/>
+                    <br></br>
+
+                    <label for="password">Password</label>
+                    <input />
+                    <br></br>
+
+                    <button type="submit" onclick="">Login</button>
+                </div>
             </>
         );
     }
