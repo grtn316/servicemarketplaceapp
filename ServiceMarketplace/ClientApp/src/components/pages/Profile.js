760985c62bfb662a2bacf6490b1dfdfada8c7920
ShawnJohnston <44795446+ShawnJohnston@users.noreply.github.com>
2024-07-16 17:28:11 -0400

    Initial profile layout

--- ServiceMarketplace/ClientApp/src/components/pages/Profile.js
+++ ServiceMarketplace/ClientApp/src/components/pages/Profile.js
@@ -10,21 +10,34 @@ export class Profile extends Component {
                     <h5 className="card-header">Business Name</h5>
                     <div className="card-body">
                         <h5 className="card-title">Location</h5>
-                        <p className="card-text">(address)</p>
+                        <p className="card-text address"></p>
 
                         <h5 className="card-title">Contact</h5>
-                        <p className="card-text">Email: </p>
-                        <p className="card-text">Phone Number: </p>
+                        <p className="card-text email">Email: </p>
+                        <p className="card-text phone-number">Phone Number: </p>
 
                         <h5 className="card-title">About</h5>
-                        <p className="card-text">(...)</p>
+                        <p className="card-text about"></p>
+                    </div>
+                    <div className="">
+                        <div className="card-body">
+                            <h5 className="card-title">Services</h5>
+                        </div>
+                        <div>
+                            <div className="card">
+                                <h5 className="card-title">Service Name</h5>
+                                <p className="">Description</p>
+                                <p className="">Rating</p>
+                            </div>
+                            <div className="card">
+                                <h5 className="card-title">Service Name</h5>
+                                <p className="">Description</p>
+                                <p className="">Rating</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="card container-sm col-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Services</h5>
-                    </div>
-                </div>
+                
             </>
         );
     }
