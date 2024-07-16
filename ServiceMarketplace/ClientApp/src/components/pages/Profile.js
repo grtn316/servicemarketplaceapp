5385f87b73f908436d524b3d23ea61ec2e28c633
ShawnJohnston <44795446+ShawnJohnston@users.noreply.github.com>
2024-07-16 17:13:30 -0400

    Initial profile page layout

--- ServiceMarketplace/ClientApp/src/components/pages/Profile.js
+++ ServiceMarketplace/ClientApp/src/components/pages/Profile.js
@@ -6,7 +6,25 @@ export class Profile extends Component {
     render() {
         return (
             <>
+                <div className="card container-sm col-8">
+                    <h5 className="card-header">Business Name</h5>
+                    <div className="card-body">
+                        <h5 className="card-title">Location</h5>
+                        <p className="card-text">(address)</p>
 
+                        <h5 className="card-title">Contact</h5>
+                        <p className="card-text">Email: </p>
+                        <p className="card-text">Phone Number: </p>
+
+                        <h5 className="card-title">About</h5>
+                        <p className="card-text">(...)</p>
+                    </div>
+                </div>
+                <div className="card container-sm col-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Services</h5>
+                    </div>
+                </div>
             </>
         );
     }
