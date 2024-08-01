39ce76444bea10fd78218dba179b84afd402a4e0
ShawnJohnston <44795446+ShawnJohnston@users.noreply.github.com>
2024-08-01 19:36:03 -0400

    Initial Service Calendar

---
+++ ServiceMarketplace/Entities/ServiceCalendar.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceMarketplace.Entities {
+    public class ServiceCalendar {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ServiceId { get; set; }
+        public List<TimeSlot> TimeSlots = new List<TimeSlot>();
+        
+        public ServiceCalendar() { }
+        public void SetAvailability(ServiceAvailability availability) {
+            CreateTimeSlots(availability);
+        }
+        private void CreateTimeSlots(ServiceAvailability availability) {
+            DateOnly dateIterator = availability.StartDate;
+            TimeOnly timeIterator = availability.StartTime;
+            TimeSpan duration = availability.Duration;
+
+            while (dateIterator <= availability.EndDate) {
+                while ((timeIterator.AddMinutes(duration.TotalMinutes)) <= availability.EndTime) {
+                    if (availability.DaysAvailable.Contains(dateIterator.Day.ToString())) {
+                        TimeSlot timeSlot = new TimeSlot(dateIterator, timeIterator, timeIterator.AddMinutes(duration.TotalMinutes), availability.Duration);
+                        this.TimeSlots.Add(timeSlot);
+                    }
+                }
+                dateIterator.AddDays(1);
+            }
+        }
+        private void ClearTimeSlots() {
+            TimeSlots.Clear();
+        }
+
+    }
+}
---
+++ ServiceMarketplace/Entities/TimeSlot.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceMarketplace.Entities {
+    public class TimeSlot {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ServiceId { get; set; }
+        [Required]
+        public int CustomerID { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public TimeSlot(DateOnly dateOnly, TimeOnly startTime, TimeOnly endTime, TimeSpan duration) {
+            IsAvailable = true;
+
+            Date = dateOnly;
+            StartTime = startTime;
+            EndTime = endTime;
+            Duration = duration;
+        }
+        public void MakeAvailable() {
+            IsAvailable = true;
+            CustomerID = 0;
+        }
+        public void MakeNotAvailable() {
+            IsAvailable = false;
+        }
+        public void MakeNotAvailable(int customerID) {
+            IsAvailable = false;
+            CustomerID = customerID;
+        }
+    }
+}
